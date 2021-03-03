588e636789188b4ce768d49b0e6d315526530100
Yueying-12N <yueying.cao@eleves.ec-nantes.fr>
2021-03-04 04:49:41 +0800

    Added 1st homework

---
+++ battle_new1.py
@@ -0,0 +1,14 @@
+import math
+robot_sizes = [1,2,3,4,5]
+person_power = 1
+def calcRobotPower (rs):
+    rp = 0
+    for i in range(1,rs+1):
+        rp = rp + i
+    return rp
+
+for rs in robot_sizes:
+    robot_power=calcRobotPower(rs)
+    people_required = math.ceil(robot_power/person_power)
+    print('Number of people required to defeat a robot of size',
+rs, 'is:', people_required)
