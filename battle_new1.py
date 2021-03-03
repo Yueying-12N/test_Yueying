c5d0e9e34b055d6061fad478c896a90f1573292a
Yueying-12N <yueying.cao@eleves.ec-nantes.fr>
2021-03-04 04:56:06 +0800

    test change content of the file

--- battle_new1.py
+++ battle_new1.py
@@ -12,3 +12,5 @@ for rs in robot_sizes:
     people_required = math.ceil(robot_power/person_power)
     print('Number of people required to defeat a robot of size',
 rs, 'is:', people_required)
+
+print('To be continued')
