ac04d4038756c4741885a8f8ae0b27e6d59e5589
Yueying-12N <yueying.cao@eleves.ec-nantes.fr>
2021-03-07 18:08:21 +0800

    add visualization and matrix manipulation

--- numpy.py
+++ numpy.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+
 out = np.asarray([1,0,1])
 print('out is',out,'type is',out.dtype)
 
@@ -42,4 +46,39 @@ print('D is ',D)
 E=A_ones*A_ones  #same as np.multiply
 print('E is ',E)
 F=np.power(A_ones, 2)  #Calculate the power
-print('F is ',F)
+
+
+'''Broadcasting'''
+data = np.random.rand(100,5)
+mu = np.mean(data,axis=0)
+print('the mean of data is',mu)
+data_c = data - mu   #despite the difference in shape
+                     #the substraction was correctly effectuated
+print('the mean of data centerd is ',np.mean(data_c))
+
+'''Matrix manipulation'''
+A = np.random.rand(3,4)
+print(A)
+A_delete = np.delete(A,3,1)
+print('delte the last column :',A_delete)
+
+# flatten() returns a copy,will not affect the original matrix
+# ravel() returns a view, will affect the original matrix when modified
+A_vector = A.flatten()
+print('translate A to vector :',A_vector)
+Ar1 = A[0]
+print('the max of row1 is:',Ar1.max())
+Ac2 = A[:,1]
+print('the min of column2 is:',Ac2.min())
+B = A.copy()
+B[0]=[0,0,0,0]
+B[1]=[1,1,1,1]
+B[2]=[1,1,1,1]
+print('B is :',B)
+print('A is :',A)
+
+
+'''Visualization'''
+A = np.random.rand(3,4)
+plt.imshow(A,interpolation='nearest',cmap='gray')
+plt.show()
